aa81f05a12d8bb138740c1976220826edc82699e
NehaNRao <nehanrao44@gmail.com>
2025-05-19 10:51:41 +0530

    Add files via upload

---
+++ ColorRemover.java
@@ -0,0 +1,43 @@
+package ColorRemover;
+
+public class ColorRemover {
+	import java.util.ArrayList;
+	import java.util.Scanner;
+
+	public class ColorRemover {
+	    public static void main(String[] args) {
+	        // Create an ArrayList to store color names
+	        ArrayList<String> colors = new ArrayList<>();
+
+	        // Add colors to the list
+	        colors.add("Red");
+	        colors.add("Blue");
+	        colors.add("Green");
+	        colors.add("Yellow");
+	        colors.add("Orange");
+
+	        // Display original list
+	        System.out.println("Original list of colors: " + colors);
+
+	        // Get the index (n) to remove from user
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the index (starting from 0) of the color to remove: ");
+	        int index = scanner.nextInt();
+
+	        // Validate index and remove element
+	        if (index >= 0 && index < colors.size()) {
+	            String removedColor = colors.remove(index);
+	            System.out.println("Removed color at index " + index + ": " + removedColor);
+	        } else {
+	            System.out.println("Invalid index. No color removed.");
+	        }
+
+	        // Display updated list
+	        System.out.println("Updated list of colors: " + colors);
+
+	        scanner.close();
+	    }
+	}
+
+
+}
