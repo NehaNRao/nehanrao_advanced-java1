0553bfb914b1e5e5bdc9707d0c39ec93321c38d8
NehaNRao <nehanrao44@gmail.com>
2025-05-19 11:18:13 +0530

    Add files via upload

---
+++ ReverseLinkedListIteration.java
@@ -0,0 +1,29 @@
+package ReverseLinkedListIteration;
+
+	import java.util.LinkedList;
+	import java.util.Iterator;
+
+	public class ReverseLinkedListIteration {
+	    public static void main(String[] args) {
+	        // Create a LinkedList and add elements
+	        LinkedList<String> colors = new LinkedList<>();
+	        colors.add("Red");
+	        colors.add("Blue");
+	        colors.add("Green");
+	        colors.add("Yellow");
+	        colors.add("Orange");
+
+	        // Display original list
+	        System.out.println("Original list: " + colors);
+
+	        // Iterate in reverse order using descendingIterator()
+	        System.out.println("Iterating in reverse order:");
+	        Iterator<String> reverseIterator = colors.descendingIterator();
+	        
+	        while (reverseIterator.hasNext()) {
+	            System.out.println(reverseIterator.next());
+	        }
+	    }
+	}
+
+
