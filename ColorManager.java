0726afee740298ddcbd61cf24d75f1546c4a933c
NehaNRao <nehanrao44@gmail.com>
2025-05-19 10:27:17 +0530

    Add files via upload

---
+++ ColorManager.java
@@ -0,0 +1,43 @@
+package ColorManager;
+import java.util.ArrayList;
+
+
+
+	public class ColorManager {
+	    public static void main(String[] args) {
+	        // Create an ArrayList to store color names
+	        ArrayList<String> colors = new ArrayList<>();
+
+	        // Add colors to the list
+	        colors.add("Red");
+	        colors.add("Blue");
+	        colors.add("Green");
+	        colors.add("Yellow");
+	        colors.add("Orange");
+
+	        // Display original list
+	        System.out.println("Original list of colors: " + colors);
+
+	        // Remove the 2nd element (index 1)
+	        if (colors.size() > 1) {
+	            String removedColor = colors.remove(1);
+	            System.out.println("Removed 2nd element: " + removedColor);
+	        } else {
+	            System.out.println("The list has fewer than 2 elements, cannot remove 2nd element.");
+	        }
+
+	        // Remove the color "Blue" if it exists
+	        boolean isBlueRemoved = colors.remove("Blue");
+	        if (isBlueRemoved) {
+	            System.out.println("The color 'Blue' was found and removed.");
+	        } else {
+	            System.out.println("The color 'Blue' was not found in the list.");
+	        }
+
+	        // Display updated list
+	        System.out.println("Updated list of colors: " + colors);
+	    }
+	}
+
+
+
