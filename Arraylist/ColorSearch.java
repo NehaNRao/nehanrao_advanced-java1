d0f61c9dfac93316633cf06b0dee95631070c009
NehaNRao <nehanrao44@gmail.com>
2025-05-20 19:47:43 +0530

    Update ColorSearch.java

--- Arraylist/ColorSearch.java
+++ Arraylist/ColorSearch.java
@@ -1,3 +1,5 @@
+/*Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
 package ArrayList;
 import java.util.ArrayList;
 
